4dd907bbc1a41ab8afb6423a7725c293087a053b
tknott95 <tknott95@hotmail.com>
2016-10-26 23:59:17 -0600

    Markers showing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+
+  //Zoom Level
+  zoom: number = 10;
+
+  // Start Position | WINDSOR COLORADO
+  lat: number = 40.466581;
+  lng: number = -104.921348;
+  // Markers
+  markers: marker[] = [
+    {
+      name: 'Fort Collins',
+      lat: 40.548776,
+      lng: -105.065905,
+      draggable: true
+    },
+    {
+      name: 'Greeley',
+      lat: 40.414137,
+      lng: -104.771171,
+      draggable: true
+    },
+    {
+      name: 'Johnstown',
+      lat: 40.349805,
+      lng: -104.949745,
+      draggable: true
+    }
+  ];
+
+
+  constructer(){
+
+  }
+  
+  
+}
+
+//Marker Type
+interface marker {
+  name?: string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
